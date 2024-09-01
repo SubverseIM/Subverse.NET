05bc8f5507cde7947b50d9ead97bb3a405a8c243
Isabelle Santin <isabelle@chosenfewsoftware.com>
2024-09-01 16:02:35 -0500

    meow

--- Subverse.Server/PeerBootstrapService.cs
+++ Subverse.Server/PeerBootstrapService.cs
@@ -75,7 +75,7 @@ internal class PeerBootstrapService : BackgroundService
                     stoppingToken.ThrowIfCancellationRequested();
 
                     // Try connection w/ 5 second timeout
-                    using (var cts = new CancellationTokenSource(TimeSpan.FromSeconds(1.0)))
+                    using (var cts = new CancellationTokenSource(TimeSpan.FromSeconds(5.0)))
                     {
                         var quicConnection = await QuicConnection.ConnectAsync(
                             new QuicClientConnectionOptions
