3f77cfa43daf8978324848a83bcf0c06d9eaa632
Isabelle Santin <isabelle@chosenfewsoftware.com>
2024-10-06 18:43:59 -0500

    meow

--- Subverse.Server/QuichePeerConnection.cs
+++ Subverse.Server/QuichePeerConnection.cs
@@ -147,12 +147,11 @@ namespace Subverse.Server
         }
 
         public async Task<SubversePeerId> CompleteHandshakeAsync(SubverseMessage message, CancellationToken cancellationToken)
-        {
-            await _connection.ConnectionEstablished.WaitAsync(cancellationToken);
-            
+        {   
             QuicheStream outboundStream = _connection.GetUnusedLocalStream();
             SendMessage(message, outboundStream);
 
+            await _connection.ConnectionEstablished.WaitAsync(cancellationToken);
             QuicheStream inboundStream = await _connection.AcceptInboundStreamAsync(cancellationToken);
 
             CancellationTokenSource newCts = new();
