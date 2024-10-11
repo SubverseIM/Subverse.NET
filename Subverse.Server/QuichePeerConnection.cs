d651c4cd2bf66ffd0f3446c89b35297babd22397
IsaMorphic <me@isamorphic.dev>
2024-10-11 14:19:40 -0500

    what the actual fuck

--- Subverse.Server/QuichePeerConnection.cs
+++ Subverse.Server/QuichePeerConnection.cs
@@ -74,15 +74,15 @@ namespace Subverse.Server
                     {
                         try
                         {
-                            bsonReader.Read();
-
                             var message = serializer.Deserialize<SubverseMessage>(bsonReader) ??
                                 throw new InvalidOperationException("Expected SubverseMessage, got malformed data instead!");
 
                             _initialMessageSource.TrySetResult(message);
                             OnMessageRecieved(new MessageReceivedEventArgs(message));
+
+                            bsonReader.Read();
                         }
-                        catch (JsonException) { await Task.Delay(75); }
+                        catch (JsonException) { await Task.Delay(150); }
 
                         cancellationToken.ThrowIfCancellationRequested();
                     }
