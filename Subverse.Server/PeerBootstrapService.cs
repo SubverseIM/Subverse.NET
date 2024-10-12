5532006506090909d3aff5ac8d42d07d7fe2c87b
Isabelle Santin <isabelle@chosenfewsoftware.com>
2024-10-12 15:55:50 -0500

    mrrp

--- Subverse.Server/PeerBootstrapService.cs
+++ Subverse.Server/PeerBootstrapService.cs
@@ -143,7 +143,7 @@ internal class PeerBootstrapService : BackgroundService
                             MaxInitialLocalBidiStreamDataSize = QuicheLibrary.MAX_BUFFER_LEN,
                             MaxInitialRemoteBidiStreamDataSize = QuicheLibrary.MAX_BUFFER_LEN,
 
-                            MaxIdleTimeout = 30_000,
+                            MaxIdleTimeout = 10_000,
                         };
 
                         clientConfig.SetApplicationProtocols("SubverseV2");
--- Subverse.Server/QuicheListenerService.cs
+++ Subverse.Server/QuicheListenerService.cs
@@ -68,7 +68,7 @@ namespace Subverse.Server
                 MaxInitialLocalBidiStreamDataSize = QuicheLibrary.MAX_BUFFER_LEN,
                 MaxInitialRemoteBidiStreamDataSize = QuicheLibrary.MAX_BUFFER_LEN,
 
-                MaxIdleTimeout = 30_000,
+                MaxIdleTimeout = 10_000,
             };
 
             serverConfig.SetApplicationProtocols("SubverseV2");
