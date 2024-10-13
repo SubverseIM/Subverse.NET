3a91dbe6faf7a90e1f99c4715c1f031e72b91c11
Isabelle Santin <isabelle@chosenfewsoftware.com>
2024-10-13 12:54:58 -0500

    okay

--- Subverse.Server/PeerBootstrapService.cs
+++ Subverse.Server/PeerBootstrapService.cs
@@ -142,7 +142,7 @@ internal class PeerBootstrapService : BackgroundService
                             MaxInitialLocalBidiStreamDataSize = QuicheLibrary.MAX_BUFFER_LEN,
                             MaxInitialRemoteBidiStreamDataSize = QuicheLibrary.MAX_BUFFER_LEN,
 
-                            MaxIdleTimeout = 20_000,
+                            MaxIdleTimeout = 30_000,
                         };
 
                         clientConfig.SetApplicationProtocols("SubverseV2");
